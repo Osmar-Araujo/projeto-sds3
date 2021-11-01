761e52c668fbc0b29ebaa2c22f45b989a4c7352f
Osmar-Araujo <araujooa@gamil.com>
2021-11-01 12:56:35 -0300

    Implementados os componentes estáticos básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Iniciado o projeto</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
